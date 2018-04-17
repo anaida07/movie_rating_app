cc9460184b25dabfd8637a26215774393834641c
Aneeta Sharma <get.aneeta@gmail.com>
2018-04-17 19:44:10 +0545

    Add config.

--- config/Config.js
+++ config/Config.js
@@ -1,4 +1,12 @@
 module.exports = {
   DB: 'mongodb://localhost/movie_rating_app',
   SECRET: 'movieratingappsecretkey',
+  FACEBOOK_APP_ID: '161708997950642',
+  FACEBOOK_APP_SECRET: '256ed3ac3bafacfc00ed7206a7cb93c3',
+  TWITTER_APP_ID: 'R6D4aFRwwSNmUxA47wwSUbusA',
+  TWITTER_APP_SECRET: 'mQJ1p08ljt7T7EUjgwxk3WCzTUzv70rfOdKSaDELqXnZG77RCQ',
+  GOOGLE_APP_ID: '635583779301-30hlunb04ehcleacm71hr7d12gqcggnt.apps.googleusercontent.com',
+  GOOGLE_APP_SECRET: '5jXr712WI9z8CjZ9tP6DLH9K',
+  LINKEDIN_APP_ID: '81vpeaes09tuvi',
+  LINKEDIN_APP_SECRET: 'tvmctPGaqHvtziTg'
 }
