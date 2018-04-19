b12fb1d3ee8c00bde9c274da089ec8ee66aadb81
Aneeta Sharma <get.aneeta@gmail.com>
2018-04-19 23:38:11 +0545

    Update config.

--- config/Config.js
+++ config/Config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  DB: 'mongodb://localhost/movie_rating_app',
+  DB: 'mongodb://movieratingappuser:movieratingappuser12345678@ds251849.mlab.com:51849/movie_rating_app',
+  // DB: 'mongodb://localhost/movie_rating_app',
   SECRET: 'movieratingappsecretkey',
   FACEBOOK_APP_ID: '161708997950642',
   FACEBOOK_APP_SECRET: '256ed3ac3bafacfc00ed7206a7cb93c3',
